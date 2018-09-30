79b7955ee34a8c42edf668bc01557be1d2dcec90
kotcher1 <annashkt@gmail.com>
2018-09-30 22:26:06 +0300

    Zeroes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+
 module.exports = function getZerosCount(number) {
-  // your implementation
+  let s = 1;
+  let fives = Math.pow(5, s);
+  let zeroes = 0;
+  for (; number >= fives; fives = Math.pow(5, ++s)) {
+    zeroes = zeroes + Math.trunc(number / fives);
+  }
+  return zeroes;
 }
